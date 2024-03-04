937ff8bab258bb9f2a51852dd4faf837d39525d2
Wolfr4me <midiscord12345@gmail.com>
2024-03-04 15:24:41 +0100

    Trabajo terminado

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -6,10 +6,15 @@ public class Main {
         CCuenta cuenta1;
         double saldoActual;
 
+        operativa_cuenta(50);
+    }
+
+    private static void operativa_cuenta(float cantidad) {
+        CCuenta cuenta1;
+        double saldoActual;
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
-
         try {
             cuenta1.retirar(2300);
         } catch (Exception e) {
